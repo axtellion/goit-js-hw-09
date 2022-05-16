f30d5ed455890c8b3502e2955b637d6bab1de275
axtellion <slark19735@gmail.com>
2022-05-16 18:11:54 +0300

    the first task end

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,30 @@
+const refs = {
+    btnStart: document.querySelector("[data-start]"),
+    btnStop: document.querySelector("[data-stop]"),
+    body: document.querySelector("body"),
+}
+
+let timerId = null;
+let color = "";
+
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+
+refs.btnStart.addEventListener("click", () => {
+    timerId = setInterval(() => {
+        color = getRandomHexColor();
+        refs.body.style.backgroundColor = color;
+    }, 1000)
+    refs.btnStart.disabled = true;
+    refs.btnStop.disabled = false;
+});
+
+refs.btnStop.addEventListener("click", () => {
+    clearInterval(timerId);
+    refs.btnStart.disabled = false;
+    refs.btnStop.disabled = true;
+});
 
